fada5f3ed2bbdcd6b289b3d34ec79a2934e7b973
mewrails <paulakeka.16.32@gmail.com>
2024-06-13 00:58:12 +0200

    menu final
    pantalla del menu animada + botones play y exit funcionales

---
+++ Assets/Scrips/MainMenu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    //boton de inicio
+    public void Play()
+    {
+        // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("escena_juego");
+    }
+
+    //cerrar el juego
+    public void Done()
+    {
+        Application.Quit();
+        //para compprobar que funciona
+        Debug.Log("Game Finished");
+    }
+}
